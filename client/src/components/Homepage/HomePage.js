d419b5c0df378e6f9a2ecc3181aca5dcbf373e85
Anna <anna.giang1999@gmail.com>
2021-03-05 22:32:22 +1100

    Added page contents to HomePage
    The component will be modified to use state, and the functions will also
    be extended to contain proper implementation.

--- client/src/components/Homepage/HomePage.js
+++ client/src/components/Homepage/HomePage.js
@@ -2,19 +2,41 @@ import React from 'react';
 
 class HomePage extends React.Component {
 
-    printHelloWorld() {
-        return "Hello World!";
-    }
+  getRemainingPoints() {
+    let points = 30;
+    // TODO: Fetch the points of this user from the database.
+    return points;
+  }
 
+  getCovidCases() {
+    let cases = 7
+    // TODO: Link to external API to fetch cases in Victoria
+    // TODO: Get the user's current location (ask permission)
+    // and get the number of cases within a 5km radius
+    return cases;
+  }
 
-    // Class Components use render() to return the JSX code
-    render() {
-        return (
-            <>
-                <h1>{this.printHelloWorld()} I'm HomePage Component</h1>
-            </>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>CovFree</h1>
+          <p>Decide what's essential for you.</p>
+        </div>
+        <div>
+          <h2>My Points</h2>
+          <p>{this.getRemainingPoints()}</p>
+        </div>
+        <div>
+          <h2>Cases near me</h2>
+          <p>{this.getCovidCases()}</p>
+        </div>
+        <div>
+          <button>Check In</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default HomePage;
