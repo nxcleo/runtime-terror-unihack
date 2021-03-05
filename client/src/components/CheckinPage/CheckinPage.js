dfe7df6668aa6fdfaf5670334da1a1923cf21bce
Aaron Jiang <nxcleo@gmail.com>
2021-03-05 21:53:07 +1100

    added qr package

--- client/src/components/CheckinPage/CheckinPage.js
+++ client/src/components/CheckinPage/CheckinPage.js
@@ -1,20 +1,131 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-class CheckinPage extends React.Component {
+import Scan from './Scan';
+import {Redirect, Route, Router, Switch} from "react-router-dom";
+import HomePage from "../Homepage/HomePage";
+import DiscoverPage from "../DiscoverPage/DiscoverPage";
+import HistoryPage from "../HistoryPage/HistoryPage";
 
-    printHelloWorld() {
-        return "Hello World!";
-    }
 
 
-    // Class Components use render() to return the JSX code
-    render() {
-        return (
-            <>
-                <h1>{this.printHelloWorld()} I'm CheckinPage Component</h1>
-            </>
-        )
-    }
+const CheckinPage = () => {
+    const [value, setValue] = useState({
+        result: '',
+        place_selected: false
+    });
+
+
+    const OnSelect = () => {
+        setValue({
+            result: "{'place_id': 'ChIJk-DqctZr1moRaUtrILlx2Hw'}",
+            place_selected: true
+        });
+        console.log(value);
+    };
+
+    const getPage = () => {
+        if (!value.place_selected) {
+            return (
+                <div>
+                    <p>wtf</p>
+                    {Scan}
+
+                    <a href="/home">
+                        <button>
+                            Cancel
+                        </button>
+                    </a>
+
+                    <button onClick={OnSelect}>
+                        Select
+                    </button>
+                </div>
+
+
+            )
+        }
+        else {
+            return (
+                <p>Selected Location: {value.result}</p>
+
+            )
+        }
+    };
+
+    return (
+        <>
+            <h1>I'm Checkin Page Component</h1>
+
+            <div>
+                {getPage()}
+            </div>
+        </>
+    )
 }
+
+// class CheckinPage extends React.Component {
+//     constructor(props) {
+//         super();
+//         this.state = {
+//             result: '',
+//             place_selected: false
+//         };
+//
+//     }
+//
+//     printHelloWorld() {
+//         return "Hello World!";
+//     }
+//
+//     OnSelect() {
+//         this.setState({
+//             result: "{'place_id': 'ChIJk-DqctZr1moRaUtrILlx2Hw'}",
+//             place_selected: true
+//         });
+//         console.log(this.state);
+//     }
+//
+//     getPage() {
+//         if (!this.state.place_selected) {
+//             return (
+//                 <div>
+//                     <p>wtf</p>
+//                     {Scan}
+//
+//                     <a href="/home">
+//                         <button>
+//                             Cancel
+//                         </button></a>
+//
+//                     <button onClick={this.OnSelect}>
+//                         Select
+//                     </button>
+//                 </div>
+//
+//
+//             )
+//         }
+//         else {
+//             return (
+//                 <p>Selected Location: {this.state.result}</p>
+//
+//             )
+//         }
+//     }
+//
+//
+//     // Class Components use render() to return the JSX code
+//     render() {
+//         return (
+//             <>
+//                 <h1>{this.printHelloWorld()} I'm Checkin Page Component</h1>
+//
+//                 <div>
+//                     {this.getPage()}
+//                 </div>
+//             </>
+//         )
+//     }
+// }
 
 export default CheckinPage;
--- client/src/components/CheckinPage/Scan.js
+++ client/src/components/CheckinPage/Scan.js
@@ -1,39 +1,38 @@
 import React from 'react';
 import QrReader from 'react-qr-reader'
-import Scan from "../CheckinPage/Scan";
 
 // Example of Class Component
 
-class Scanner extends React.Component {
+class Scan extends React.Component {
     state = {
         result: 'No result'
     }
 
     handleScan = data => {
-        if (data) {
+        if (data && data[0] === "{" && data[data.length - 1] === "}") {
             this.setState({
                 result: data
             })
+
         }
     }
     handleError = err => {
         console.error(err)
     }
+
     render() {
         return (
             <div>
                 <QrReader
-                    delay={300}
+                    delay={250}
                     onError={this.handleError}
                     onScan={this.handleScan}
                     style={{ width: '100%' }}
                 />
                 <p>{this.state.result}</p>
-                <div>
-                    {Scan}
-                </div>
+
             </div>
         )
     }
 }
-export default Scanner;
+export default Scan;
